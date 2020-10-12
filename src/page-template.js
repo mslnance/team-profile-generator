6edd73a24ae2f60d456e1f3d9a42f504cfbc211c
Symphony <55611595+lnance6@users.noreply.github.com>
2020-10-12 14:55:06 -0700

    removed comment for template

--- src/page-template.js
+++ src/page-template.js
@@ -83,7 +83,7 @@ module.exports = (templateData ) => {
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Team Page</title>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"> -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"> 
       
     </head>
   
